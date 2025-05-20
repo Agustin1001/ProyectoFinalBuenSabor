0dca525adcecb15c18d93021ebe8e2f7cab22e75
Agustin <agustinmarinlautaro1001@gmail.com>
2025-05-19 21:16:41 -0300

    parte Agustin

--- src/Localidad.java
+++ src/Localidad.java
@@ -1,2 +1,17 @@
 public class Localidad {
+    private String nombre;
+    private Provincia provincia;
+
+    public Localidad() {
+    }
+
+    public Localidad(String nombre, Provincia provincia) {
+        this.nombre = nombre;
+        this.provincia = provincia;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + ", provincia=" + provincia;
+    }
 }
